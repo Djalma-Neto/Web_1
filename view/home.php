a318c22c96b1739da03809c1bcfe28da208a4ea8
Guilherme <guilherme.kaliter@gmail.com>
2021-02-14 12:12:34 -0300

    dashboard components added and aligned

--- view/home.php
+++ view/home.php
@@ -1,5 +1,5 @@
 <?php
-    session_start();
+session_start();
 ?>
 <!DOCTYPE html>
 <html>
@@ -10,11 +10,36 @@
     <title>APPLICATION</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/home.css">
+    <link rel="stylesheet" href="../css/home.css">
 </head>
 
 <body>
-    teste
+    <div class="navbar">
+        <div class="navBody">
+            <img class="menu" src="../css/img/menu.svg" alt="">
+            <h3>
+                ESQUADRITEC
+            </h3>
+            <div id="divBusca">
+                <input type="text" id="txtBusca" placeholder="Buscar..." />
+                <img src="../css/img/search.svg" id="btnBusca" alt="Buscar" />
+            </div>
+            <img class="user" src="../css/img/account.svg" alt="">
+        </div>
+    </div>
+    <div>
+        <div class="cards">
+            <div class="card Orcamentos"></div>
+            <div class="card Clientes"></div>
+            <div class="card Materiais"></div>
+        </div>
+    </div>
+    <div class="Fab">
+        <span>+</span>
+    </div>
+    <div class="graph">
+
+    </div>
 </body>
 
 </html>
