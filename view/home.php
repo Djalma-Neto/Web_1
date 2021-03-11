da432fb92b9212e829b8232b170c27c8ca7165c3
Djalma-Neto <djalma7743@gmail.com>
2021-03-11 00:44:13 -0300

    .

--- view/home.php
+++ view/home.php
@@ -25,31 +25,31 @@ if(!$_SESSION['user']){
         </div>
         <div class="colunm-2">
             <div class="menu_options">
-                <a href="https://esquadritec.herokuapp.com/usuario/new_user.php" class="menu_Button">
+                <a href="https://esquadritec.herokuapp.com/view/usuario/new_user.php" class="menu_Button">
                     <img style="width:20px;" src="../css/img/add.svg" alt="cadastrar funcionario">
                     <div style="padding-botton: 5px;">FUNCIONÁRIO</div>
                 </a>
 
-                <a href="https://esquadritec.herokuapp.com/cliente/new_cliente.php" class="menu_Button">
+                <a href="https://esquadritec.herokuapp.com/view/cliente/new_cliente.php" class="menu_Button">
                     <img style="width:20px;" src="../css/img/add.svg" alt="cadastrar cliente">
                     <div style="padding-botton: 5px;">CLIENTE</div>
                 </a>
 
-                <a href="https://esquadritec.herokuapp.com/usuario/new_user.php" class="menu_Button">
+                <a href="https://esquadritec.herokuapp.com/view/usuario/new_user.php" class="menu_Button">
                     <img style="width:20px;" src="../css/img/add.svg" alt="cadastrar orçamentos">
                     <div style="padding-botton: 5px;">ORÇAMENTO</div>
                 </a>
 
-                <a href="https://esquadritec.herokuapp.com/materiais/new_material.php" class="menu_Button">
+                <a href="https://esquadritec.herokuapp.com/view/materiais/new_material.php" class="menu_Button">
                     <img style="width:20px;" src="../css/img/add.svg" alt="cadastrar material">
                     <div style="padding-botton: 5px;">MATERIAL</div>
                 </a>
 
-                <a href="https://esquadritec.herokuapp.com/view/linha/new_linha.php" class="menu_Button">
+                <a href="https://esquadritec.herokuapp.com/view/view/linha/new_linha.php" class="menu_Button">
                     <img style="width:20px;" src="../css/img/add.svg" alt="cadastrar funcionario">
                     <div style="padding-botton: 5px;">LINHA</div>
                 </a>
-                <a href="https://esquadritec.herokuapp.com/view/modelo/new_modelo.php" class="menu_Button">
+                <a href="https://esquadritec.herokuapp.com/view/view/modelo/new_modelo.php" class="menu_Button">
                     <img style="width:20px;" src="../css/img/add.svg" alt="cadastrar funcionario">
                     <div style="padding-botton: 5px;">MODELO</div>
                 </a>
