2bb269fbb39abebb0358b380edc54aa0e208a960
Djalma-Neto <djalma7743@gmail.com>
2021-03-14 19:02:22 -0300

    .

---
+++ view/index.php
@@ -0,0 +1,16 @@
+<?php
+    include_once("../models/connect.php");
+    $_SESSION['error'] = '';
+    $_SESSION['user'] = '';
+    $_SESSION['sucess'] = '';
+    $_SESSION['materiais'] = '';
+    $_SESSION["modelos"] = '';
+    $_SESSION['linhas'] = '';
+    $_SESSION["clientes"] = '';
+    getAllMaterial();
+    getAllModelo();
+    getAllLinha();
+    getAllCliente();
+
+    header("Location: ./view/login.php");
+?>
