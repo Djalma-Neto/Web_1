c30d74524f7fcce1ed29368c5e6d1bc0efd1e4f4
Guilherme <guilherme.kaliter@gmail.com>
2021-03-15 20:57:00 -0300

    style added to clientes.php

--- view/cliente/clientes.php
+++ view/cliente/clientes.php
@@ -15,6 +15,7 @@ if(!$_SESSION['user']){
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="../../css/materiais.css">
+    <link rel="stylesheet" href="../style.css">
 </head>
 
 <body>
