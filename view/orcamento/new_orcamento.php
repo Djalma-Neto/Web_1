7122ae46efe48ac22180b877f199695ffedb56a8
Guilherme <guilherme.kaliter@gmail.com>
2021-03-23 21:57:22 -0300

    asas

--- view/orcamento/new_orcamento.php
+++ view/orcamento/new_orcamento.php
@@ -15,7 +15,7 @@ getAllCliente();
     <title>ESQUADRITEC</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="../../css/new_orcamento.css">
+    <link rel="stylesheet" href="../../css/new_user.css">
 </head>
 
 <body>
