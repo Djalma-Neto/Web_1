e8ffd89fc68b6c52c80e883e3e4b67ac89b64f27
Djalma-Neto <djalma7743@gmail.com>
2021-03-24 23:38:44 -0300

    .

--- view/linha/linhas.php
+++ view/linha/linhas.php
@@ -95,14 +95,14 @@ if (!$_SESSION['user']) {
                     <?php
                     $linhas = $_SESSION['linhas'];
                     for ($x = 0; $x < count($linhas); $x++) {
-                        $linha = str_replace(' ', '\n', $linhas[$x]->linha);
+                        $linha = str_replace(' ', ' ', $linhas[$x]->linha);
                         echo
                         "<tr>
                             <td class='nome'>{$linhas[$x]->linha}</td>
                             <td class='acao'>
                                 <form class='list-component' action='./update_linha.php' method='POST'>
                                     <input type='hidden' name='id' value={$linhas[$x]->id}>
-                                    <input type='hidden' name='linha' value=".nl2br($linha).">
+                                    <input type='hidden' name='linha' value=".$linha.">
                                     <button type='submit' class='option'><img class='icon' src='../../css/img/update.svg'></button>
                                 </form>
                                 <form class='list-component' action='../../models/linha/del_linha.php' method='POST'>
